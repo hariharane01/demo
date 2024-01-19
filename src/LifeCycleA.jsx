2aa8ac948cb58f47e29d4f7cb20b007363a436fc
hariharane01 <hariharane01@gmail.com>
2024-01-20 01:01:18 +0530

    update

--- src/LifeCycleA.jsx
+++ src/LifeCycleA.jsx
@@ -1,26 +1,26 @@
 import React, { Component } from 'react';
 
 class LifeCycle extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state={
+        this.state = {
             name: "hariharane",
         };
         console.log("LifeCycleA Constructor ");
 
 
     }
-    
-    componentDidMount(){
+
+    componentDidMount() {
         console.log("LifeCycleA componentDidMount")
     }
 
-    static getDerivedStateFromProps(){
+    static getDerivedStateFromProps() {
         console.log("LifeCycleA getDerivedStateFromProps ")
         return null;
 
     }
-   
+
     render() {
         console.log("LifeCycleA render")
         return (
