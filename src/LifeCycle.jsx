a5160201ceb2a8557af1747093377daf71b82a5e
hariharane01 <hariharane01@gmail.com>
2024-01-20 00:43:23 +0530

    first commit

---
+++ src/LifeCycle.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class LifeCycle extends Component {
+    constructor(){
+        
+    }
+
+
+
+
+
+
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default LifeCycle;
